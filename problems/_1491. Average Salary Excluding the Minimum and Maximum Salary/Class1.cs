0cd98b0d211f5d7ac9fa705b1bd88e16bd0249aa
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-14 19:11:40 +0300

    added solution for 1491. Average Salary Excluding the Minimum and Maximum Salary

---
+++ problems/_1491. Average Salary Excluding the Minimum and Maximum Salary/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace _1491._Average_Salary_Excluding_the_Minimum_and_Maximum_Salary;
+
+public class Solution
+{
+    public double Average(int[] salary)
+    {
+        return salary.OrderBy(i => i).Skip(1).SkipLast(1).Average();
+    }
+}
