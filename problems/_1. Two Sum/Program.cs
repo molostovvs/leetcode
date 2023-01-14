50c9774a3ee4e741c5e71dad5f41128fac468f31
Viktor <86685532+steeldebil@users.noreply.github.com>
2023-01-14 23:26:19 +0300

    added solution for 1. Two Sum

---
+++ problems/_1. Two Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
+
+public class Solution 
+{
+    public int[] TwoSum(int[] nums, int target) 
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = nums.Length - 1; j > i; j--)
+            {
+                if (nums[i] + nums[j] == target)
+                    return new[] { i, j };
+            }
+        }
+
+        return null;
+    }
+}
