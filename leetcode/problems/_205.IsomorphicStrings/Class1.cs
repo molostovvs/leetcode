0c3f9dc95138569f1e01fe3a64a22f35a18bf460
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-14 18:24:48 +0300

    added solution 205. Isomorphic Strings

---
+++ leetcode/problems/_205.IsomorphicStrings/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using FluentAssertions;
+using NUnit.Framework;
+
+public class Program
+{
+    static void Main(string[] args) {}
+}
+
+public class Solution
+{
+    public bool IsIsomorphic(string s, string t)
+    {
+        var d = new Dictionary<char, char>();
+        for (var i = 0; i < s.Length; i++)
+            if ((d.ContainsKey(s[i]) && d[s[i]] != t[i]) || (!d.ContainsKey(s[i]) && d.ContainsValue(t[i])))
+                return false;
+            else
+                d.TryAdd(s[i], t[i]);
+        return true;
+    }
+}
+
+[TestFixture]
+public class Tests
+{
+    private static Solution s = new();
+
+    [TestCase("egg", "add", true)]
+    [TestCase("paper", "title", true)]
+    [TestCase("foo", "bar", false)]
+    [TestCase("door", "room", true)]
+    [TestCase("door", "flat", false)]
+    [TestCase("badc", "baba", false)]
+    public static void Examples(string s1, string s2, bool expected)
+    {
+        var result = s.IsIsomorphic(s1, s2);
+        result.Should().Be(expected);
+    }
+}
