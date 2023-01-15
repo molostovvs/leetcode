41668fe5a591522aae217ea7aceee721bff6ef0d
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-15 17:06:08 +0300

    added O(n) solution for 1. Two Sum

--- problems/_1. Two Sum/Program.cs
+++ problems/_1. Two Sum/Program.cs
@@ -1,22 +1,33 @@
-﻿public class Program
+﻿using System.Collections;
+
+public class Program
 {
-    static void Main(string[] args)
-    {
-        
-    }
+    static void Main(string[] args) {}
 }
 
-public class Solution 
+public class Solution
 {
-    public int[] TwoSum(int[] nums, int target) 
+    public int[] TwoSum(int[] nums, int target)
     {
-        for (int i = 0; i < nums.Length; i++)
+        /*for (int i = 0; i < nums.Length; i++) // O(n^2)
         {
             for (int j = nums.Length - 1; j > i; j--)
             {
                 if (nums[i] + nums[j] == target)
                     return new[] { i, j };
             }
+        }
+
+        return null;*/
+
+        var table = new Dictionary<int, int>(); // (number, position in array)
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var need = target - nums[i];
+            if (table.ContainsKey(need))
+                return new[] { table[need], i };
+            table.TryAdd(nums[i], i);
         }
 
         return null;
