f24be45f971206b4bb7231c246b5e60e2b666995
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-16 07:12:08 +0300

    added solution for 876. Middle of the Linked List

---
+++ problems/_876. Middle of the Linked List/Class1.cs
@@ -0,0 +1,31 @@
+﻿namespace _876._Middle_of_the_Linked_List;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public ListNode MiddleNode(ListNode head)
+    {
+        if (head == null)
+            return null;
+        var arr = new List<ListNode>();
+
+        while (head != null)
+        {
+            arr.Add(head);
+            head = head.next;
+        }
+
+        return arr[arr.Count / 2];
+    }
+}
