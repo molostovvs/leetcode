8e82e17d5a12259f376a0fb900083ff0544ca012
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-17 08:53:42 +0300

    added solution for 409. Longest Palindrome

---
+++ problems/_409. Longest Palindrome/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace _409._Longest_Palindrome;
+
+public class Solution
+{
+    public int LongestPalindrome(string s)
+    {
+        var dict = new Dictionary<char, int>(); //can be replaced with set [time O(n), space O(1)]
+        foreach (var c in s)
+            if (dict.ContainsKey(c))
+                dict[c]++;
+            else
+                dict.Add(c, 1);
+
+        var result = 0;
+
+        foreach (var kv in dict)
+            if (kv.Value % 2 == 0)
+                result += kv.Value;
+            else if (result % 2 == 0)
+                result += kv.Value;
+            else
+                result += kv.Value - 1;
+
+        return result;
+    }
+}
