57e4b59d679a6361f7a59df473ac319bfba911db
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-17 09:16:09 +0300

    added solution for 1822. Sign of the Product of an Array

---
+++ problems/_1822. Sign of the Product of an Array/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace _1822._Sign_of_the_Product_of_an_Array;
+
+public class Solution
+{
+    public int ArraySign(int[] nums)
+    {
+        var negatives = 0; // [time O(n), space O(1)]
+        foreach (var n in nums)
+            if (n < 0)
+                negatives++;
+            else if (n == 0)
+                return 0;
+
+        return negatives % 2 == 0 ? 1 : -1;
+    }
+}
