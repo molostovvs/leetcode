1323b191c37f44b96393012a7cb877a69b9a8f82
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-17 09:31:42 +0300

    added solution for 1502. Can Make Arithmetic Progression From Sequence

---
+++ problems/_1502. Can Make Arithmetic Progression From Sequence/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace _1502._Can_Make_Arithmetic_Progression_From_Sequence;
+
+public class Solution
+{
+    public bool CanMakeArithmeticProgression(int[] arr)
+    {
+        Array.Sort(arr);
+        var diff = arr[1] - arr[0];
+        for (var i = 2; i < arr.Length; i++)
+            if (arr[i] - arr[i - 1] != diff)
+                return false;
+
+        return true;
+    }
+}
