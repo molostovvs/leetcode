dcfc354ce222f9b7bbf1c440654a6782a46df8d7
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-19 08:52:01 +0300

    added solution for 278. First Bad Version

---
+++ problems/_278. First Bad Version/Class1.cs
@@ -0,0 +1,27 @@
+﻿namespace _278._First_Bad_Version;
+
+public class VersionControl
+{
+    public bool IsBadVersion(int version)
+        => true;
+}
+
+public class Solution : VersionControl
+{
+    public int FirstBadVersion(int n)
+    {
+        var left = 1;
+        var right = n;
+
+        while (left <= right)
+        {
+            var mid = left + (right - left) / 2;
+            if (IsBadVersion(mid))
+                right = mid - 1;
+            else if (!IsBadVersion(mid))
+                left = mid + 1;
+        }
+
+        return left;
+    }
+}
