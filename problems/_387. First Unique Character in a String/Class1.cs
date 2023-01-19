3808b6a5f2760bf5fb94ac5f17ce91135cd573a1
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-19 09:13:03 +0300

    added solution for 387. First Unique Character in a String

---
+++ problems/_387. First Unique Character in a String/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace _387._First_Unique_Character_in_a_String;
+
+public class Solution
+{
+    public int FirstUniqChar(string s)
+    {
+        var dict = new Dictionary<char, int>(); //char - number of occurences in string
+        foreach (var c in s)
+            if (!dict.TryAdd(c, 1))
+                dict[c]++;
+
+        for (var i = 0; i < s.Length; i++)
+            if (dict[s[i]] == 1)
+                return i;
+
+        return -1;
+    }
+}
