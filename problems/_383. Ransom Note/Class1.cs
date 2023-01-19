6da40141c408960e48f86f8ef65771a37c5d47e2
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-19 09:25:46 +0300

    added solution for 383. Ransom Note

---
+++ problems/_383. Ransom Note/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace _383._Ransom_Note;
+
+public class Solution
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        var dict = new Dictionary<char, int>();
+
+        foreach (var c in magazine)
+            if (!dict.TryAdd(c, 1))
+                dict[c]++;
+
+        foreach (var c in ransomNote)
+            if (dict.ContainsKey(c) && dict[c] > 0)
+                dict[c]--;
+            else
+                return false;
+        return true;
+    }
+}
