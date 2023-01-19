186af3fcedf899321944ce2874a6c86994be4f8b
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-19 13:03:00 +0300

    added solution for 1672. Richest Customer Wealth

---
+++ problems/_1672. Richest Customer Wealth/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace _1672._Richest_Customer_Wealth;
+
+public class Solution
+{
+    public int MaximumWealth(int[][] accounts)
+        => accounts.Select(customer => customer.Sum()).Max();
+}
