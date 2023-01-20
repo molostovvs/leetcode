04d6b239480210a91a9d635f3a8fbcc6f4c38d6e
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-20 11:57:43 +0300

    added solution for 203. Remove Linked List Elements

---
+++ problems/_203. Remove Linked List Elements/Class1.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace _203._Remove_Linked_List_Elements;
+
+public class Program
+{
+    public static void Main() {}
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public ListNode RemoveElements(ListNode head, int val)
+    {
+        var dummy = new ListNode(-100, head);
+        var prev = dummy;
+
+        while (head is not null)
+        {
+            if (head.val == val)
+                prev.next = head.next;
+            else
+                prev = head;
+
+            head = head.next;
+        }
+
+        return dummy.next;
+    }
+}
+
+[TestFixture]
+public class Tests
+{
+    private static Solution s = new();
+
+    [Test]
+    public static void Example()
+    {
+        var head = new ListNode(1, new ListNode(3, new ListNode(2)));
+        var result = s.RemoveElements(head, 2);
+        var expected = new ListNode(1, new ListNode(3));
+        result.Should().BeEquivalentTo(expected);
+    }
+}
