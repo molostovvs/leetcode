cf2fb6740afa5616d671c31235afa1dd5183d716
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-20 10:05:45 +0300

    added solution for 1572. Matrix Diagonal Sum

---
+++ problems/_1572. Matrix Diagonal Sum/Class1.cs
@@ -0,0 +1,21 @@
+﻿namespace _1572._Matrix_Diagonal_Sum;
+
+//my solution [O(n), O(1)]
+public class Solution
+{
+    public int DiagonalSum(int[][] mat)
+    {
+        var sum = 0;
+        var n = mat.GetLength(0);
+
+        for (var i = 0; i < n; i++)
+            sum += mat[i][i];
+
+        for (var j = n; j > 0; j--)
+            sum += mat[n - j][j - 1];
+
+        sum -= n % 2 != 0 ? mat[n / 2][n / 2] : 0;
+
+        return sum;
+    }
+}
