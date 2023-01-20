49cd7d4bdfdecd53f0d3cf16db722c9809742fce
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-20 10:18:32 +0300

    added solution for 141. Linked List Cycle

---
+++ problems/_141. Linked List Cycle/Class1.cs
@@ -0,0 +1,33 @@
+﻿namespace _141._Linked_List_Cycle;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int x)
+    {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution
+{
+    public bool HasCycle(ListNode head)
+    {
+        var slow = head;
+        var fast = head;
+
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast)
+                return true;
+        }
+
+        return false;
+    }
+}
