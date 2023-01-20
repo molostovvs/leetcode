4d90ba319bc907662f4e039a9d814efe410ca53b
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-20 13:24:54 +0300

    unable to solve 491. Non-decreasing Subsequences

---
+++ problems/_491. Non-decreasing Subsequences/Class1.cs
@@ -0,0 +1,8 @@
+﻿namespace _491._Non_decreasing_Subsequences;
+
+//unable to solve this
+public class Solution
+{
+    public IList<IList<int>> FindSubsequences(int[] nums)
+        => throw new NotImplementedException();
+}
