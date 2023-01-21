d6fde9e8a118e1ab3ce522efcc6d7d53bfe5ec76
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-21 14:30:01 +0300

    added solution for 1678. Goal Parser Interpretation

---
+++ problems/_1678. Goal Parser Interpretation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+public class Solution
+{
+    public string Interpret(string command)
+    {
+        var sb = new StringBuilder();
+
+        for (var i = 0; i < command.Length;)
+            if (command[i] == 'G')
+            {
+                sb.Append("G");
+                i++;
+            }
+            else
+            {
+                i++;
+                if (command[i] == ')')
+                {
+                    sb.Append("o");
+                    i++;
+                }
+                else
+                {
+                    sb.Append("al");
+                    i += 3;
+                }
+            }
+
+        return sb.ToString();
+    }
+}
