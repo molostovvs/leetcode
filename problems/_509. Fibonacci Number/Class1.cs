ab2ec21657e6966af9a0e4591b69d08345e2b857
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-22 12:09:14 +0300

    added solution for 509. Fibonacci Number

---
+++ problems/_509. Fibonacci Number/Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace _509._Fibonacci_Number;
+
+public class Solution
+{
+    public int Fib(int n)
+    {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+
+        var res = -1;
+        var preprev = 0;
+        var prev = 1;
+
+        for (var i = 2; i <= n; i++)
+        {
+            res = preprev + prev;
+            preprev = prev;
+            prev = res;
+        }
+
+        return res;
+    }
+}
