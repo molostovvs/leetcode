867353b2ba004be1bba3e4b5c0483cccbc75a71c
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-22 16:28:38 +0300

    added solution for 20. Valid Parentheses

---
+++ problems/_20. Valid Parentheses/Class1.cs
@@ -0,0 +1,34 @@
+﻿namespace _20._Valid_Parentheses;
+
+public class Solution
+{
+    private char _result;
+
+    public bool IsValid(string inp)
+    {
+        var s = new Stack<char>();
+
+        foreach (var p in inp)
+            if (p is '(' or '[' or '{')
+            {
+                s.Push(p);
+            }
+            else
+            {
+                char opening;
+                if (!s.TryPop(out opening))
+                    return false;
+                switch (opening)
+                {
+                    case '(' when p is ')':
+                    case '[' when p is ']':
+                    case '{' when p is '}':
+                        continue;
+                    default:
+                        return false;
+                }
+            }
+
+        return s.Count == 0;
+    }
+}
