ad29f1b92c60b4580f53f4aff580983cf8e311b8
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-22 14:08:22 +0300

    added solution for 70. Climbing Stairs

---
+++ problems/_70. Climbing Stairs/Class1.cs
@@ -0,0 +1,37 @@
+﻿namespace _70._Climbing_Stairs;
+
+/*// [O(n), O(n)]
+public class Solution
+{
+    public int ClimbStairs(int n)
+    {
+        var dp = new int[n + 1];
+        dp[n] = 1;
+        dp[n - 1] = 1;
+
+        for (var i = n - 2; i >= 0; i--)
+            dp[i] = dp[i + 1] + dp[i + 2];
+
+        return dp[0];
+    }
+}*/
+
+// [O(n), O(1)]
+public class Solution
+{
+    public int ClimbStairs(int n)
+    {
+        var prepre = 1;
+        var pre = 1;
+
+        while (n - 1 > 0)
+        {
+            var cur = prepre + pre;
+            prepre = pre;
+            pre = cur;
+            n--;
+        }
+
+        return pre;
+    }
+}
