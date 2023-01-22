218e52685dd0be67db20aed93ca90b8d617c0ef8
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-22 11:04:01 +0300

    added solution for 709. To Lower Case

---
+++ problems/_709. To Lower Case/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+/*// Library Solution [O(n), O(n)]
+public class Solution
+{
+    public string ToLowerCase(string s)
+    {
+        return s.ToLowerInvariant();
+    }
+}*/
+
+//Custom solution [O(n), O(n)]
+public class Solution
+{
+    public string ToLowerCase(string s)
+    {
+        var sb = new StringBuilder();
+        foreach (var c in s)
+            if (c >= 'A' && c <= 'Z')
+                sb.Append((char)('a' - 'A' + c));
+            else
+                sb.Append(c);
+
+        return sb.ToString();
+    }
+}
