0af317f63a2c010e66037698bbd4ef09bd10506e
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-25 19:45:27 +0300

    added solution for 22. Generate Parentheses

---
+++ problems/_22. Generate Parentheses/Class1.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+public class Program
+{
+    public static void Main() {}
+}
+
+//NeetCode [O(?), O(?)]
+public class Solution
+{
+    public IList<string> GenerateParenthesis(int n)
+    {
+        var st = new Stack<string>();
+
+        return Backtrack(0, 0).ToList();
+
+        IEnumerable<string> Backtrack(int open, int closed)
+        {
+            if (open == closed && closed == n)
+                yield return string.Join("", st.Select(s => s).Reverse());
+
+            if (open < n)
+            {
+                st.Push("(");
+                foreach (var s in Backtrack(open + 1, closed))
+                    yield return s;
+                st.Pop();
+            }
+
+            if (closed < open)
+            {
+                st.Push(")");
+                foreach (var s in Backtrack(open, closed + 1))
+                    yield return s;
+                st.Pop();
+            }
+        }
+    }
+}
+
+[TestFixture]
+public class Tests
+{
+    private static Solution s = new Solution();
+
+    [TestCase(1, new[] { "()" })]
+    public static void Example(int n, IList<string> expected)
+    {
+        var result = s.GenerateParenthesis(n);
+        result.Should().BeEquivalentTo(expected, opt => opt.WithStrictOrdering());
+    }
+}
