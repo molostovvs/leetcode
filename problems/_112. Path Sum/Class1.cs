920a85c3a11f8bdcb1e26cff2541601bb481613a
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-25 12:16:34 +0300

    added solution for 112. Path Sum

---
+++ problems/_112. Path Sum/Class1.cs
@@ -0,0 +1,28 @@
+﻿public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public bool HasPathSum(TreeNode root, int targetSum, int currSum = 0)
+    {
+        if (root is null)
+            return false;
+
+        if (root.left is null && root.right is null && currSum + root.val == targetSum)
+            return true;
+
+        return HasPathSum(root.left, targetSum, currSum + root.val)
+            || HasPathSum(root.right, targetSum, currSum + root.val);
+    }
+}
