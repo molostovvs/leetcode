8a4c1e45e834fc03030b8dbaa1d438f4803ac914
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-25 23:12:29 +0300

    added solution for 739. Daily Temperatures

---
+++ problems/_739. Daily Temperatures/Class1.cs
@@ -0,0 +1,43 @@
+﻿//naive solution [o(n^2), O(1)]
+
+public class Solution
+{
+    public int[] DailyTemperatures(int[] temps)
+    {
+        var ans = new int[temps.Length];
+
+        for (var i = 0; i < ans.Length; i++)
+        {
+            for (var j = i + 1; j < ans.Length; j++)
+                if (temps[j] > temps[i])
+                {
+                    ans[i] = j - i;
+                    break;
+                }
+        }
+
+        return ans;
+    }
+}
+
+// spied solution [O(n), O(n)]
+public class SolutionFast
+{
+    public int[] DailyTemperatures(int[] temps)
+    {
+        var ans = new int[temps.Length];
+        var st = new Stack<int>();
+
+        for (var i = ans.Length - 1; i >= 0; i--)
+        {
+            while (st.Count != 0 && temps[st.Peek()] <= temps[i])
+                st.Pop();
+
+            if (st.Count != 0)
+                ans[i] = st.Peek() - i;
+            st.Push(i);
+        }
+
+        return ans;
+    }
+}
