7bd6b20b39dd29403c4a44eb439fd7e6e39c53ea
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-25 13:45:19 +0300

    added solution for 150. Evaluate Reverse Polish Notation

---
+++ problems/_150. Evaluate Reverse Polish Notation/Class1.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+public class Program
+{
+    public static void Main() {}
+}
+
+public class Solution
+{
+    public int EvalRPN(string[] tokens)
+    {
+        var s = new Stack<int>();
+        var operations = new Dictionary<string, Func<int, int, int>>();
+        operations.Add("+", (x, y) => y + x);
+        operations.Add("-", (x, y) => y - x);
+        operations.Add("*", (x, y) => y * x);
+        operations.Add("/", (x, y) => y / x);
+
+        foreach (var str in tokens)
+            if (str != "+" && str != "-" && str != "*" && str != "/")
+                s.Push(int.Parse(str));
+            else
+                s.Push(operations[str].Invoke(s.Pop(), s.Pop()));
+
+        return s.Pop();
+    }
+}
+
+[TestFixture]
+public class Tests
+{
+    private static Solution s = new Solution();
+
+    [TestCase(new[] { "4", "13", "5", "/", "+" }, 6)]
+    public static void Example(string[] tokens, int expected)
+    {
+        var result = s.EvalRPN(tokens);
+        result.Should().Be(expected);
+    }
+}
