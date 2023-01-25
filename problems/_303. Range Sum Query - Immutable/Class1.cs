f6b19cee86a2d74cee3ff6c5939fceaa2509c5a8
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-25 09:56:25 +0300

    adedd solution for 303. Range Sum Query - Immutable

---
+++ problems/_303. Range Sum Query - Immutable/Class1.cs
@@ -0,0 +1,25 @@
+﻿public class NumArray
+{
+    private int[] _nums;
+
+    public NumArray(int[] nums)
+        => _nums = nums;
+
+    public int SumRange(int left, int right)
+    {
+        var result = 0;
+        while (left <= right)
+        {
+            result += _nums[left];
+            left++;
+        }
+
+        return result;
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.SumRange(left,right);
+ */
