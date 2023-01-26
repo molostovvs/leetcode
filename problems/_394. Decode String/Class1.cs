17700c81eb0d787bcc6d6d5f98c56e4fb6293e30
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-26 12:26:09 +0300

    added solution for 394. Decode string - but was unable to solve and understand task

---
+++ problems/_394. Decode String/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+
+public class Program
+{
+    public static void Main() {}
+}
+
+public class Solution
+{
+    public string DecodeString(string s)
+    {
+        var pointer = 0;
+        var sb = Dfs(s, ref pointer);
+        return sb.ToString();
+    }
+
+    StringBuilder Dfs(string s, ref int pointer)
+    {
+        StringBuilder sbNumber = new();
+        StringBuilder sbWord = new();
+        StringBuilder sbResult = new();
+        if (pointer >= s.Length)
+            return sbResult;
+        var n = 0;
+        while (pointer < s.Length && char.IsDigit(s[pointer]))
+        {
+            sbNumber.Append(s[pointer]);
+            pointer++;
+        }
+
+        if (sbNumber.Length > 0)
+            n = Convert.ToInt32(sbNumber.ToString());
+        if (n > 0)
+        {
+            if (s[pointer] == '[')
+            {
+                pointer++;
+                sbWord = Dfs(s, ref pointer);
+            }
+
+            for (int i = 0; i < n; i++)
+                sbResult.Append(sbWord.ToString());
+        }
+        else
+        {
+            while (pointer < s.Length && char.IsLetter(s[pointer]))
+            {
+                sbWord.Append(s[pointer++]);
+                // pointer++;
+            }
+
+            if (pointer >= s.Length - 1 || s[pointer] == ']')
+                return sbWord;
+
+            if (char.IsDigit(s[pointer]))
+            {
+                return sbWord.Append(Dfs(s, ref pointer));
+                // return sbWord;
+            }
+        }
+
+        if (pointer < s.Length - 1)
+        {
+            pointer++;
+            sbResult.Append(Dfs(s, ref pointer));
+        }
+
+        return sbResult;
+    }
+}
+
+[TestFixture]
+public class Tests
+{
+    private static Solution s = new Solution();
+
+    [TestCase("3[a]2[bc]", "aaabcbc")]
+    public static void Example(string encoded, string expected)
+    {
+        var result = s.DecodeString(encoded);
+        result.Should().Be(expected);
+    }
+}
