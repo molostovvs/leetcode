29cba6947a7eb052504711df8669a4394bd303d9
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-26 12:47:21 +0300

    added solution for 700. Search in a Binary Search Tree

---
+++ problems/_700. Search in a Binary Search Tree/Class1.cs
@@ -0,0 +1,31 @@
+﻿public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public TreeNode SearchBST(TreeNode root, int val)
+    {
+        if (root is null)
+            return null;
+
+        if (root.val == val)
+            return root;
+
+        if (val < root.val)
+            return SearchBST(root.left, val);
+        if (val > root.val)
+            return SearchBST(root.right, val);
+        return null;
+    }
+}
