3b43358e60bb73c02cb81cb18a3d53ac03a6b0de
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-26 13:00:19 +0300

    added solution for 701. Insert into a Binary Search Tree

---
+++ problems/_701. Insert into a Binary Search Tree/Class1.cs
@@ -0,0 +1,49 @@
+﻿public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public TreeNode InsertIntoBST(TreeNode root, int val)
+    {
+        if (root is null)
+            return new TreeNode(val);
+
+        if (val < root.val)
+        {
+            if (root.left is not null)
+            {
+                InsertIntoBST(root.left, val);
+            }
+            else
+            {
+                root.left = new TreeNode(val);
+                return root;
+            }
+        }
+        else
+        {
+            if (root.right is not null)
+            {
+                InsertIntoBST(root.right, val);
+            }
+            else
+            {
+                root.right = new TreeNode(val);
+                return root;
+            }
+        }
+
+        return root;
+    }
+}
