f6423ed1c71d70992b1d3612ded987f76e3a39a1
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-26 13:46:17 +0300

    added solution for 35. Search Insert Position

---
+++ problems/_35. Search Insert Position/Class1.cs
@@ -0,0 +1,25 @@
+﻿public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        var left = 0;
+        var right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            if (left == right)
+                break;
+            var mid = left + (right - left) / 2;
+            if (target < nums[mid])
+                right = mid - 1;
+            else if (target > nums[mid])
+                left = mid + 1;
+            else if (target == nums[mid])
+                return mid;
+        }
+
+        if (nums[left] < target)
+            return left + 1;
+        return left;
+    }
+}
