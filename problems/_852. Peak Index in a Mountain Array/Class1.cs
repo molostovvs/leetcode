5e8e780dd6ba61d23957a52e378b9e21b0fe7475
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-26 16:21:23 +0300

    added solution for 852. Peak Index in a Mountain Array

---
+++ problems/_852. Peak Index in a Mountain Array/Class1.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public int PeakIndexInMountainArray(int[] arr)
+    {
+        var left = 0;
+        var right = arr.Length - 1;
+
+        while (left <= right)
+        {
+            var mid = left + (right - left) / 2;
+            if (arr[mid] >= arr[mid + 1])
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+
+        return left;
+    }
+}
