f6e775a29bc170c719f683f792c98a5ffd4fbcf6
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-27 16:18:18 +0300

    updated timer for 278. First Bad Version

--- problems/_278. First Bad Version/Class1.cs
+++ problems/_278. First Bad Version/Class1.cs
@@ -6,6 +6,7 @@ public class VersionControl
         => true;
 }
 
+/*//first attempt 10 min
 public class Solution : VersionControl
 {
     public int FirstBadVersion(int n)
@@ -19,6 +20,27 @@ public class Solution : VersionControl
             if (IsBadVersion(mid))
                 right = mid - 1;
             else if (!IsBadVersion(mid))
+                left = mid + 1;
+        }
+
+        return left;
+    }
+}*/
+
+//second attempt 3min
+public class Solution : VersionControl
+{
+    public int FirstBadVersion(int n)
+    {
+        var left = 0;
+        var right = n;
+
+        while (left <= right)
+        {
+            var mid = left + (right - left) / 2;
+            if (IsBadVersion(mid))
+                right = mid - 1;
+            else
                 left = mid + 1;
         }
 
