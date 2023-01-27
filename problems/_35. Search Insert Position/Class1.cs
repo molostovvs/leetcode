4fb4f6a502da5e15b2d45ced38a48570f4aeb14d
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-27 16:32:52 +0300

    updated timer for 35. Search Insert Position

--- problems/_35. Search Insert Position/Class1.cs
+++ problems/_35. Search Insert Position/Class1.cs
@@ -1,4 +1,5 @@
-﻿public class Solution
+﻿/*//first attempt
+public class Solution
 {
     public int SearchInsert(int[] nums, int target)
     {
@@ -21,5 +22,30 @@
         if (nums[left] < target)
             return left + 1;
         return left;
+    }
+}*/
+
+//second attempt 5 min
+public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        var left = 0;
+        var right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            var mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        if (left >= nums.Length)
+            return left;
+        return nums[left] < target ? left + 1 : left;
     }
 }
