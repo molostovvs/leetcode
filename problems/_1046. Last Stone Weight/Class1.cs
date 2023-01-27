e1f08f00c957fae0265717100fe40de0fab6a54f
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-27 10:30:08 +0300

    added solution for 1046. Last Stone Weight

---
+++ problems/_1046. Last Stone Weight/Class1.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int LastStoneWeight(int[] stones)
+    {
+        var q = new PriorityQueue<int, int>();
+        foreach (var s in stones)
+            q.Enqueue(s, -s); //(s, -s) for Max Heap, so biggest values at the top of queue
+
+        while (q.Count > 1)
+        {
+            var s1 = q.Dequeue();
+            var s2 = q.Dequeue();
+            var res = s1 - s2;
+            if (res != 0)
+                q.Enqueue(res, -res);
+        }
+
+        return q.Count > 0 ? q.Dequeue() : 0;
+    }
+}
