70b4ef097c8a4f25db03f4d5dff345dfd4324f2f
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-27 19:22:56 +0300

    added solution for 26. Remove Duplicates from Sorted Array

---
+++ problems/_26. Remove Duplicates from Sorted Array/Class1.cs
@@ -0,0 +1,25 @@
+﻿//first attempt 10 min [O(n), O(1)]
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        var slow = 0;
+        var fast = 0;
+
+        while (fast < nums.Length)
+        {
+            if (nums[slow] == nums[fast])
+            {
+                fast++;
+            }
+            else
+            {
+                nums[slow] = nums[fast - 1];
+                slow++;
+                nums[slow] = nums[fast];
+            }
+        }
+
+        return slow + 1;
+    }
+}
