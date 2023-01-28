74f482d42dc05c75095a4c7b29c8a12286eeb7c3
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-28 09:17:25 +0300

    added solution for 66. Plus One

---
+++ problems/_66. Plus One/Class1.cs
@@ -0,0 +1,43 @@
+﻿public class Solution
+{
+    public int[] PlusOne(int[] digits)
+    {
+        var s = new Stack<int>();
+        var overhead = 0;
+
+        for (var i = digits.Length - 1; i >= 0; i--)
+        {
+            var n = digits[i];
+            if (i == digits.Length - 1)
+            {
+                if (n + 1 == 10)
+                {
+                    overhead = 1;
+                    s.Push(0);
+                }
+                else
+                {
+                    digits[digits.Length - 1] += 1;
+                    return digits;
+                }
+            }
+            else
+            {
+                if (n + overhead == 10)
+                {
+                    overhead = 1;
+                    s.Push(0);
+                }
+                else
+                {
+                    s.Push(n + overhead);
+                    overhead = 0;
+                }
+            }
+        }
+
+        if (overhead > 0)
+            s.Push(overhead);
+        return s.ToArray();
+    }
+}
