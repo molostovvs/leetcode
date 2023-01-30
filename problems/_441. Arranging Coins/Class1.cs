7da95dfad4bd5ac1a713035ed58d4e2b6e3b38e1
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-30 10:29:04 +0300

    added solution for 441. Arranging Coins

---
+++ problems/_441. Arranging Coins/Class1.cs
@@ -0,0 +1,40 @@
+﻿//my solution [O(log n), O(1)]
+public class MySolution
+{
+    public int ArrangeCoins(int n)
+    {
+        var steps = 1;
+        while (n > 0)
+        {
+            n -= steps;
+            steps++;
+        }
+
+        return n < 0 ? steps - 2 : steps - 1;
+    }
+}
+
+//binary search [O(log n), O(1)]
+public class Solution
+{
+    public int ArrangeCoins(int n)
+    {
+        long left = 1, right = n;
+
+        while (left <= right)
+        {
+            long mid = left + (right - left) / 2;
+            long curr = mid * (mid + 1) / 2;
+
+            if (curr == n)
+                return (int)mid;
+
+            if (n < curr)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+
+        return (int)right;
+    }
+}
