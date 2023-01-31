fbd6f23d9d6f9bc7eab94cded6627a17656291b6
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-01-31 12:05:11 +0300

    added solution for 1608. Special Array With X Elements Greater Than or Equal X

---
+++ problems/_1608. Special Array With X Elements Greater Than or Equal X/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public int SpecialArray(int[] nums)
+    {
+        Array.Sort(nums);
+        var left = 0;
+        var right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            var mid = left + (right - left) / 2;
+            var n = nums.Length - mid;
+
+            if (mid > 0 && nums[mid] >= n && nums[mid - 1] < n)
+                return n;
+            if (mid == 0 && nums[mid] >= n)
+                return n;
+
+            if (nums[mid] < n)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return -1;
+    }
+}
