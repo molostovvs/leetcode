4936cfa49dfc776299c21e3481061aff3ae6c5dc
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-02-02 16:56:09 +0300

    added solution for 1346. Check If N and Its Double Exist

---
+++ problems/_1346. Check If N and Its Double Exist/Program.cs
@@ -0,0 +1,29 @@
+﻿/*// naive [O(n^2), O(1)]
+public class NaiveSolution
+{
+    public bool CheckIfExist(int[] arr)
+    {
+        for (var i = 0; i < arr.Length; i++)
+            for (var j = i + 1; j < arr.Length; j++)
+                if (arr[i] * 2 == arr[j] || arr[j] * 2 == arr[i])
+                    return true;
+        return false;
+    }
+}*/
+
+public class Solution
+{
+    public bool CheckIfExist(int[] arr)
+    {
+        var d = new Dictionary<int, int>();
+
+        for (var i = 0; i < arr.Length; i++)
+            d[arr[i]] = i;
+
+        for (var i = 0; i < arr.Length; i++)
+            if (d.ContainsKey(arr[i] * 2) && d[arr[i] * 2] != i)
+                return true;
+
+        return false;
+    }
+}
