5f88c5d388935b1aeb0400d469d6fadafcb398d4
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-02-03 12:05:52 +0300

    added solution for 633. Sum of Square Numbers

---
+++ problems/_633. Sum of Square Numbers/Program.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+public class Program
+{
+    public static void Main() {}
+}
+
+//first attempt 10 min [O(n), O(1)]
+public class Solution
+{
+    public bool JudgeSquareSum(int c)
+    {
+        long left = 0;
+        long right = (long)Math.Sqrt(c);
+
+        while (left <= right)
+        {
+            var prod = left * left + right * right;
+            if (prod == c)
+                return true;
+
+            if (prod > c)
+                right--;
+            else
+                left++;
+        }
+
+        return false;
+    }
+}
+
+[TestFixture]
+public class Tests
+{
+    private static Solution s = new();
+
+    [TestCase(5, true)]
+    [TestCase(3, false)]
+    [TestCase(17, true)]
+    [TestCase(1, true)]
+    [TestCase(1000000, true)]
+    [TestCase(2147483600, true)]
+    public static void Example(int i, bool expected)
+    {
+        var result = s.JudgeSquareSum(i);
+        result.Should().Be(expected);
+    }
+}
