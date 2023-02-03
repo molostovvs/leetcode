f15bde58f1de5ea337a9de6cf91978bff36b1b78
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-02-03 12:27:29 +0300

    added solution for 617. Merge Two Binary Trees

---
+++ problems/_617. Merge Two Binary Trees/Class1.cs
@@ -0,0 +1,34 @@
+﻿public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+//first attempt recursive 6 min [O(n), O(1)]
+public class Solution
+{
+    public TreeNode MergeTrees(TreeNode root1, TreeNode root2)
+    {
+        TreeNode node;
+        if (root1 is null && root2 is null)
+            return null;
+        if (root1 is null)
+            node = root2;
+        else if (root2 is null)
+            node = root1;
+        else
+            node = new TreeNode(root1.val + root2.val);
+
+        node.left = MergeTrees(root1?.left, root2?.left);
+        node.right = MergeTrees(root1?.right, root2?.right);
+        return node;
+    }
+}
