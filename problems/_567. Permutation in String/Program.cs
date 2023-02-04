8d260116379b2546e117af756b3bea88a0576e25
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-02-04 10:43:21 +0300

    updated timer for 567. Permutation in String

--- problems/_567. Permutation in String/Program.cs
+++ problems/_567. Permutation in String/Program.cs
@@ -6,7 +6,7 @@ public class Program
     public static void Main() {}
 }
 
-public class Solution
+public class OldSolution
 {
     public bool CheckInclusion(string s1, string s2)
     {
@@ -50,6 +50,46 @@ public class Solution
 
         return true;
     }
+}
+
+//second attempt 15 min [O(m + n), O(alphabet)]
+public class Solution
+{
+    public bool CheckInclusion(string s1, string s2)
+    {
+        var d1 = new Dictionary<char, int>();
+        foreach (var ch in s1)
+            if (!d1.TryAdd(ch, 1))
+                d1[ch]++;
+
+        var d2 = new Dictionary<char, int>();
+
+        for (var i = 0; i < s2.Length; i++)
+        {
+            if (i < s1.Length)
+            {
+                if (!d2.TryAdd(s2[i], 1))
+                    d2[s2[i]]++;
+                continue;
+            }
+
+            if (DictionariesAreTheSame(d1, d2))
+                return true;
+
+            if (d2[s2[i - s1.Length]] == 1)
+                d2.Remove(s2[i - s1.Length]);
+            else
+                d2[s2[i - s1.Length]]--;
+
+            if (!d2.TryAdd(s2[i], 1))
+                d2[s2[i]]++;
+        }
+
+        return DictionariesAreTheSame(d1, d2);
+    }
+
+    private bool DictionariesAreTheSame(Dictionary<char, int> d1, Dictionary<char, int> d2)
+        => d1.All(kv => d2.ContainsKey(kv.Key) && d2[kv.Key] == kv.Value);
 }
 
 [TestFixture]
