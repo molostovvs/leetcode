61eaa00d108c1739834677123d737f34e6528eab
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-02-05 17:45:12 +0300

    updated timer for 21. Merge Two Sorted Lists

--- problems/_21. Merge Two Sorted Lists/Class1.cs
+++ problems/_21. Merge Two Sorted Lists/Class1.cs
@@ -18,7 +18,7 @@ public class ListNode
     }
 }
 
-public static class Solution
+public static class OldSolution
 {
     public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
     {
@@ -55,5 +55,51 @@ public static class Solution
         var list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
 
         var x = MergeTwoLists(list1, list2);
+    }
+}
+
+//second attempt 8 min [O(n+m), O(1)]
+public class Solution
+{
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        var dummy = new ListNode();
+        var tail = dummy;
+
+        if (list1 is null && list2 is null)
+            return null;
+
+        while (list1 is not null || list2 is not null)
+        {
+            if (list1 is not null && list2 is not null)
+            {
+                if (list1.val <= list2.val)
+                {
+                    tail.next = list1;
+                    tail = tail.next;
+                    list1 = list1.next;
+                }
+                else
+                {
+                    tail.next = list2;
+                    tail = tail.next;
+                    list2 = list2.next;
+                }
+            }
+
+            if (list1 is null)
+            {
+                tail.next = list2;
+                break;
+            }
+
+            if (list2 is null)
+            {
+                tail.next = list1;
+                break;
+            }
+        }
+
+        return dummy.next;
     }
 }
