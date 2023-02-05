49a387e160bd66181f674479147db8e10df0cf56
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-02-05 19:52:32 +0300

    updated timer for 509. Fibonacci Number

--- problems/_509. Fibonacci Number/Class1.cs
+++ problems/_509. Fibonacci Number/Class1.cs
@@ -1,6 +1,6 @@
 ﻿namespace _509._Fibonacci_Number;
 
-public class Solution
+public class OldSolution
 {
     public int Fib(int n)
     {
@@ -21,5 +21,28 @@ public class Solution
         }
 
         return res;
+    }
+}
+
+//second attempt 4 min [O(n), O(1)]
+public class Solution
+{
+    public int Fib(int n)
+    {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        var prepre = 0;
+        var pre = 1;
+        var cur = 0;
+        for (var i = 2; i <= n; i++)
+        {
+            cur = pre + prepre;
+            prepre = pre;
+            pre = cur;
+        }
+
+        return cur;
     }
 }
