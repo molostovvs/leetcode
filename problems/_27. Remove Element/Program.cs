7b3799011e3b0d4a441d6680b65a2e84b89553e0
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-02-05 19:32:01 +0300

    added solution for 27. Remove Element

---
+++ problems/_27. Remove Element/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        var end = nums.Length - 1;
+        for (var i = 0; i <= end; i++)
+            if (nums[i] == val)
+                for (; end >= i; end--)
+                    if (nums[end] != val || end == i)
+                    {
+                        (nums[i], nums[end]) = (nums[end], nums[i]);
+                        end--;
+                        break;
+                    }
+
+        return nums.Length - (nums.Length - 1 - end);
+    }
+}
