274b7b94056ac319b9fdf0ab7b76b25c2e58167d
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-02-06 12:24:13 +0300

    added solution for 46. Permutations

---
+++ problems/_46. Permutations/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public IList<IList<int>> Permute(int[] nums)
+    {
+        var res = new List<IList<int>>();
+        Helper(res, new List<int>(), nums);
+        return res;
+    }
+
+    private void Helper(List<IList<int>> res, List<int> temp, int[] nums)
+    {
+        if (temp.Count == nums.Length)
+            res.Add(new List<int>(temp));
+        else
+            foreach (var t in nums)
+                if (!temp.Contains(t))
+                {
+                    temp.Add(t);
+                    Helper(res, temp, nums);
+                    temp.RemoveAt(temp.Count - 1);
+                }
+    }
+}
