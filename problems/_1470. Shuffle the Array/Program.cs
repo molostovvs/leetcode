1ef14d64791b4a376660636ce837bf701c95ffe7
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-02-06 09:57:07 +0300

    added solution for 1470. Shuffle the Array

---
+++ problems/_1470. Shuffle the Array/Program.cs
@@ -0,0 +1,15 @@
+﻿//first attempt 4 min [O(n), O(1)]
+public class Solution
+{
+    public int[] Shuffle(int[] nums, int n)
+    {
+        var res = new int[nums.Length];
+        for (int left = 0, right = nums.Length / 2; right < nums.Length; left++, right++)
+        {
+            res[left * 2] = nums[left];
+            res[left * 2 + 1] = nums[right];
+        }
+
+        return res;
+    }
+}
