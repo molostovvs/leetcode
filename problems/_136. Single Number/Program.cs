b3f7cf0763a47f26815915b0acacbaab8be71d7e
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-02-06 16:16:19 +0300

    added solution for 136. Single Number

---
+++ problems/_136. Single Number/Program.cs
@@ -0,0 +1,27 @@
+﻿//naive solution [O(n), O(n)]
+public class NaiveSolution
+{
+    public int SingleNumber(int[] nums)
+    {
+        var d = new Dictionary<int, byte>();
+
+        foreach (var i in nums)
+            if (!d.TryAdd(i, 1))
+                d[i]++;
+
+        return d.First(kv => kv.Value == 1).Key;
+    }
+}
+
+//fuck this problem in O(n) time
+public class Solution
+{
+    public int SingleNumber(int[] nums)
+    {
+        var x = 0;
+        foreach (var n in nums)
+            x ^= n;
+
+        return x;
+    }
+}
