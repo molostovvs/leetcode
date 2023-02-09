f38e58d52f9dbb40f473e5ff5d5bc631da5609b8
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-02-09 18:23:05 +0300

    added solution for 190. Reverse Bits

---
+++ problems/_190. Reverse Bits/Class1.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public uint reverseBits(uint n)
+    {
+        var res = 0u;
+
+        for (var i = 0; i < 32; i++)
+        {
+            res <<= 1;
+            if ((n & 1) == 1)
+                res++;
+            n >>= 1;
+        }
+
+        return res;
+    }
+}
