4f2d722de062d5f460591876be6caa026360ef5f
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-02-09 13:01:32 +0300

    added solution for 198. House Robber

---
+++ problems/_198. House Robber/Class1.cs
@@ -0,0 +1,18 @@
+﻿//first attempt 5 min [O(n), O(1)]
+public class Solution
+{
+    public int Rob(int[] nums)
+    {
+        var prepre = 0;
+        var pre = 0;
+
+        foreach (var num in nums)
+        {
+            var cur = Math.Max(prepre + num, pre);
+            prepre = pre;
+            pre = cur;
+        }
+
+        return pre;
+    }
+}
