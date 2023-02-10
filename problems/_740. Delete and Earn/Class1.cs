4fce1a8dafe3d14e5f2ef57f4a1cb340ac97f1c5
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-02-10 17:10:30 +0300

    added solution for 740. Delete and Earn

---
+++ problems/_740. Delete and Earn/Class1.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+public class Program
+{
+    public static void Main() {}
+}
+
+public class Solution
+{
+    public int DeleteAndEarn(int[] nums)
+    {
+        var buckets = new int[10001];
+        foreach (var n in nums)
+            buckets[n] += n;
+
+        var take = 0;
+        var skip = 0;
+
+        foreach (var b in buckets)
+        {
+            var takeCur = skip + b;
+            var skipCur = Math.Max(skip, take);
+            take = takeCur;
+            skip = skipCur;
+        }
+
+        return Math.Max(take, skip);
+    }
+}
+
+[TestFixture]
+public class Tests
+{
+    private static Solution s = new();
+
+    [TestCase(new[] { 3, 1 }, 4)]
+    [TestCase(new[] { 3, 4, 2 }, 6)]
+    [TestCase(new[] { 2, 2, 3, 3, 3, 4 }, 9)]
+    [TestCase(new[] { 1, 1, 1, 2, 4, 5, 5, 5, 6 }, 18)]
+    public static void Example(int[] nums, int expected)
+    {
+        var result = s.DeleteAndEarn(nums);
+        result.Should().Be(expected);
+    }
+}
