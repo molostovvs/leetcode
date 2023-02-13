2edc09b06382e28b720b0ca058437cba1118103e
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-02-13 14:18:22 +0300

    added solution for 1567. Maximum Length of Subarray With Positive Product

---
+++ problems/_1567. Maximum Length of Subarray With Positive Product/Class1.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+public class Program
+{
+    public static void Main() {}
+}
+
+public class Solution
+{
+    public int GetMaxLen(int[] nums)
+    {
+        var pos = 0;
+        var neg = 0;
+
+        var ans = 0;
+        foreach (var n in nums)
+        {
+            if (n == 0)
+            {
+                pos = 0;
+                neg = 0;
+            }
+            else if (n > 0)
+            {
+                pos++;
+                neg = neg == 0 ? 0 : neg + 1;
+            }
+            else
+            {
+                var temp = pos;
+                pos = neg == 0 ? 0 : neg + 1;
+                neg = temp + 1;
+            }
+
+            ans = Math.Max(ans, pos);
+        }
+
+        return ans;
+    }
+}
+
+[TestFixture]
+public class Tests
+{
+    private static Solution s = new();
+
+    [TestCase(new[] { 1, -2, -3, 4 }, 4)]
+    [TestCase(new[] { 0, 1, -2, -3, -4 }, 3)]
+    [TestCase(new[] { 0, -1, 0 }, 0)]
+    [TestCase(new[] { 0, 1, 0 }, 1)]
+    [TestCase(new[] { 0, 1, 1 }, 2)]
+    [TestCase(new[] { 0, 1, -1 }, 1)]
+    [TestCase(new[] { 0, 0, 0, 0 }, 0)]
+    [TestCase(new[] { -1, -1, -1, -1 }, 4)]
+    [TestCase(new[] { -1, -1, -1, 1 }, 3)]
+    [TestCase(new[] { 1, 2, -6, 4 }, 2)]
+    public static void Example(int[] nums, int expected)
+    {
+        var result = s.GetMaxLen(nums);
+        result.Should().Be(expected);
+    }
+}
