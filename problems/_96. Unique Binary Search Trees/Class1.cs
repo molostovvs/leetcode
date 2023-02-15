bf769e4ca735d7412cc1d7aaf5cb1d8ebd5c474c
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-02-15 11:07:13 +0300

    added solution for 96. Unique Binary Search Trees

---
+++ problems/_96. Unique Binary Search Trees/Class1.cs
@@ -0,0 +1,16 @@
+﻿//first attempt (spied) 35 min [O(n), O(n)]
+public class Solution
+{
+    public int NumTrees(int n)
+    {
+        var dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (var level = 2; level <= n; level++)
+            for (var root = 1; root <= level; root++)
+                dp[level] += dp[level - root] * dp[root - 1];
+
+        return dp[^1];
+    }
+}
