0de99140da93aaf4b4e146a15e5bad0d17b8abd8
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-02-20 10:15:22 +0300

    added solution for 64. Minimum Path Sum

---
+++ problems/_64. Minimum Path Sum/Class1.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public int MinPathSum(int[][] grid)
+    {
+        for (var r = 0; r < grid.GetLength(0); r++)
+            for (var c = 0; c < grid[0].GetLength(0); c++)
+                if (r == 0 && c == 0)
+                    continue;
+                else
+                    grid[r][c] += Math.Min(r - 1 >= 0 ? grid[r - 1][c] : int.MaxValue, c - 1 >= 0 ? grid[r][c - 1] : int.MaxValue);
+
+        return grid[^1][^1];
+    }
+}
