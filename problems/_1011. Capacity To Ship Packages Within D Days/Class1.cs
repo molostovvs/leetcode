5dd8faa165e2ce522f2caaa2e6290593a44df50c
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-02-22 16:41:41 +0300

    added solution for 1011. Capacity To Ship Packages Within D Days

---
+++ problems/_1011. Capacity To Ship Packages Within D Days/Class1.cs
@@ -0,0 +1,33 @@
+﻿//first attempt 30 min (spied) [O(nlogn), O(1)]
+public class Solution
+{
+    public int ShipWithinDays(int[] weights, int days)
+    {
+        var low = weights.Max();
+        var high = weights.Sum();
+
+        while (low < high)
+        {
+            var mid = (low + high) / 2;
+            var curCap = 0;
+            var shipNum = 1;
+
+            foreach (var w in weights)
+            {
+                curCap += w;
+                if (curCap > mid)
+                {
+                    curCap = w;
+                    shipNum++;
+                }
+            }
+
+            if (shipNum > days)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+
+        return low;
+    }
+}
