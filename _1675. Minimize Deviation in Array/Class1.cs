a16f5eaa8bffef7a56d82fe6adfdbb77002e08c7
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-02-24 12:18:13 +0300

    added solution for 1675. Minimize Deviation in Array

---
+++ _1675. Minimize Deviation in Array/Class1.cs
@@ -0,0 +1,33 @@
+﻿public class Solution
+{
+    public int MinimumDeviation(int[] nums)
+    {
+        int minValue = int.MaxValue;
+        int minDeviation = int.MaxValue;
+        PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if ((nums[i] & 1) != 0) 
+                nums[i] = (nums[i] * 2);
+
+            minValue = Math.Min(minValue, nums[i]);
+            pq.Enqueue(nums[i], -nums[i]);
+        }
+
+        while ((pq.Peek() & 1) == 0)
+        {
+            int maxValue = pq.Dequeue();
+            minDeviation = Math.Min(minDeviation, maxValue - minValue);
+
+            if ((maxValue & 1) == 0)
+            {
+                int newValue = (maxValue >> 1);
+                pq.Enqueue(newValue, -newValue);
+                minValue = Math.Min(minValue, newValue);
+            }
+        }
+
+        return Math.Min(minDeviation, pq.Peek() - minValue);
+    }
+}
