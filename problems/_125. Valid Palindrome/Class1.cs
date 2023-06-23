e77e57d661aa7f8fdf7dfabe409e89d6d21fd776
Viktor Molostov <86685532+steeldebil@users.noreply.github.com>
2023-06-24 00:05:03 +0300

    update timer for 125. Valid Palindrome

--- problems/_125. Valid Palindrome/Class1.cs
+++ problems/_125. Valid Palindrome/Class1.cs
@@ -1,22 +1,51 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
 
-public class Program
-{
-    public static void Main() {}
-}
-
-public class Solution
+public class FirstSolution
 {
     public bool IsPalindrome(string s)
     {
-        var arr = s.ToCharArray().Where(c => char.IsLetterOrDigit(c)).Select(c => char.ToLowerInvariant(c)).ToArray();
+        var arr = s.ToCharArray()
+                   .Where(c => char.IsLetterOrDigit(c))
+                   .Select(c => char.ToLowerInvariant(c))
+                   .ToArray();
 
         for (int l = 0, r = arr.Length - 1; l <= r; l++, r--)
             if (arr[l] != arr[r])
                 return false;
 
         return true;
+    }
+}
+
+//10 min [O(n), O(1)]
+public class SecondSolution
+{
+    public bool IsPalindrome(string s)
+    {
+        var left = 0;
+        var right = s.Length - 1;
+
+        while (left < right)
+        {
+            if (char.IsLetterOrDigit(s[left]) && char.IsLetterOrDigit(s[right]))
+            {
+                if (char.ToLowerInvariant(s[left]) != char.ToLowerInvariant(s[right]))
+                    return false;
+                left++;
+                right--;
+            }
+            else if (!char.IsLetterOrDigit(s[left]))
+            {
+                left++;
+            }
+            else if (!char.IsLetterOrDigit(s[right]))
+            {
+                right--;
+            }
+        }
+
+        return char.ToLowerInvariant(s[left]) == char.ToLowerInvariant(s[right]);
     }
 }
 
