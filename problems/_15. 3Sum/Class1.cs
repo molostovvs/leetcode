8030b41f6c3ddec7b0533ee1ac39bd985989d38e
Victor Molostov <86685532+ConcreteEcho@users.noreply.github.com>
2024-05-01 13:18:44 +0300

    upd timer for 15. 3Sum

--- problems/_15. 3Sum/Class1.cs
+++ problems/_15. 3Sum/Class1.cs
@@ -3,7 +3,7 @@ using NUnit.Framework;
 
 public class Program
 {
-    public static void Main() {}
+    public static void Main() { }
 }
 
 //[O(n^2), O(1)]
@@ -40,6 +40,78 @@ public class Solution
                         medium++;
                 }
             }
+        }
+
+        return res;
+    }
+}
+
+//25 min [O(n^3), O(1)] - TLE
+public class SecondBadSolution
+{
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        var res = new List<IList<int>>();
+
+        var set = new HashSet<(int, int, int)>();
+
+        for (var i = 0; i <= nums.Length - 3; i++)
+            for (var j = i + 1; j <= nums.Length - 2; j++)
+                for (var k = j + 1; k <= nums.Length - 1; k++)
+                    if (nums[i] + nums[j] + nums[k] == 0)
+                    {
+                        var candidate = new[] { nums[i], nums[j], nums[k] };
+
+                        Array.Sort(candidate);
+
+                        if (set.Add((candidate[0], candidate[1], candidate[2])))
+                            res.Add(candidate);
+                    }
+
+        return res;
+    }
+}
+
+//40 min spied [O(n^2), O(1)] - not sure about space complexity
+//since it depends on sorting algorithm
+public class SecondSolution
+{
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        var res = new List<IList<int>>();
+
+        Array.Sort(nums);
+
+        int start = 0, left, right, target;
+
+        while (start < nums.Length - 2)
+        {
+            target = nums[start] * -1;
+            left = start + 1;
+            right = nums.Length - 1;
+
+            while (left < right)
+            {
+                if (nums[left] + nums[right] > target)
+                    right--;
+                else if (nums[left] + nums[right] < target)
+                    left++;
+                else
+                {
+                    var solution = new[] { nums[start], nums[left], nums[right] };
+                    res.Add(solution);
+
+                    while (left < right && nums[left] == solution[1])
+                        left++;
+                    while (left < right && nums[right] == solution[2])
+                        right--;
+                }
+            }
+
+            var curStart = nums[start];
+
+            while (start < nums.Length - 2 && nums[start] == curStart)
+                start++;
         }
 
         return res;
